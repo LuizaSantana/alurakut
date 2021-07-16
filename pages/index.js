6686e98caafb54136306405af681d442fa69dea9
LuizinhaABraba <lufsantana15@gmail.com>
2021-07-16 16:06:51 -0300

    inicio do alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,78 @@
-import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import MainGrid from '../src/componentes/maingrid'
+import Box from '../src/componentes/Box'
+import {AlurakutMenu, OrkutNostalgicIconSet} from '../src/lib/AlurakutCommons';
+import { ProfileRelationsBoxWrapper} from '../src/componentes/ProfileRelations';
+
+function ProfileSidebar(props) {
+  console.log (props);
+  return (
+    <Box>
+         <img src={`https://github.com/${props.githubUser}.png`} style= {{ borderRadius:'8px'}} />
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+     const githubUser = 'LuizaSantana';
+     const pessoasFavoritas = [
+       'juunegreiros',
+       'omariosouto', 
+       'peas',
+       'rafaballerini', 
+       'marcobrunodev',
+       'felipefialho', 
+     ]
+
+
+  return (
+      <>
+    <AlurakutMenu />
+  <MainGrid>
+  
+    {/* <Box style="grid-area: profileArea;"> */}
+    <div className="profileArea" style={{ gridArea: 'profileArea'}}>
+      <ProfileSidebar githubUser= {githubUser} />
+    </div>
+    
+    <div className="welcomeArea" style={{ gridArea: 'welcomeArea'}}>
+       <Box>
+         <h1 className="title">
+         Bem vindo
+         </h1>
+
+
+         <OrkutNostalgicIconSet />
+       </Box>
+    </div>
+    
+    <div className="RelationsArea" style={{ gridArea: 'RelationsArea'}}>
+      <ProfileRelationsBoxWrapper>
+        <h2 className="smallTitle">
+         Pessoas da comunidade ({pessoasFavoritas.length})
+         </h2>
+
+         <ul>
+
+         {pessoasFavoritas.map((itemAtual)=>{
+
+          return (
+            <li>
+             <a href={`/users/${itemAtual}`} key={itemAtual}>
+               <img src={`https://github.com/${itemAtual}.png`} />
+               <span>{itemAtual}</span>
+               </a>
+               </li>
+          )
+         })}
+         </ul>
+      </ProfileRelationsBoxWrapper>
+      <Box>
+         Comunidades
+      </Box>
+    </div>
+
+  </MainGrid>
+  </>
+  )
 }
---
+++ src/componentes/maingrid/index.js
@@ -0,0 +1,27 @@
+/*MainGrid */
+import styled from 'styled-components'
+
+const MainGrid = styled.main`
+   width: 100%;
+   grid-gap: 10px;
+   margin-left: auto;
+   margin-right: auto;
+   max-width: 500px;
+   padding: 16px;
+    .profileArea {
+      display: none;
+      @media(Min-width: 860px) {
+        display: block;
+      }
+    }
+
+@media(min-width: 860px) {
+   max-width: 1110px;
+   display: grid;
+   grid-template-areas: 
+   "profileArea welcomeArea RelationsArea";
+   grid-template-columns: 160px 1fr 312px;
+}
+`;
+
+export default MainGrid;
